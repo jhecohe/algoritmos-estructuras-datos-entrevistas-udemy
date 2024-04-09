4d9c998cff36610f204a48d16f2ca46ed267c59d
jhecohe <jhecohe@gmail.com>
2024-04-09 06:53:29 -0500

    Solved isUnique and TwoSum

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -1,5 +1,8 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_unique;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /*
  * Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
  *
@@ -9,6 +12,16 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_un
 public class IsUnique {
 
   public boolean isUnique(String s) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    
+    Set<Character> unique = new HashSet<>();
+    
+    for (int i=0; i < s.length(); i++) {
+        Character c = s.charAt(i);
+        if (unique.contains(c)) {
+            return false;
+        }
+        unique.add(c);
+    }
+    return true;
   }
 }
--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSum.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSum.java
@@ -1,5 +1,7 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._02_two_sum;
 
+import java.util.HashMap;
+
 /*
  * Dado un array de números enteros y un target, retorna los índices de dos
  * números para los que la suma de ambos sea igual al target.
@@ -14,10 +16,41 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._02_two_s
  * Ejemplo 2:
  *  Input: nums = [9,2,5,6], target = 100
  *  Output: null
+ * 
+ *  9: 2,5,6 ->  return result
+ *  2: 5,6 ->  return result
  */
 public class TwoSum {
 
   public int[] twoSum(int[] nums, int target) {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+    // Definimos un map para almacenar los numeros recorridos
+    // y su posicion en el array
+    HashMap<Integer, Integer> posicions = new HashMap<>();
+
+    for (int i = 0; i < nums.length -1; i++) {
+      
+      // Validamos si el complemento para encontrar el target
+      // se encuentra en el mapa
+
+      int complement = target - nums[i];
+      if(posicions.containsKey(complement)){
+        return new int[] {posicions.get(complement), i};
+      }
+      // Si no se encuentra el complemento, se agrega el numero 
+      // y su posicion
+      posicions.put(nums[i], i);
+    }
+
+    return null;
+
+    // Big O N^2
+    // 
+    // for (int i = 0; i < nums.length; i++) {
+    //   for (int j = i+1; j < nums.length; j++) {
+    //     if ((nums[i] + nums[j]) == target) return new int[] {i, j};
+    //   }
+    // }
+    // return null;
   }
 }
