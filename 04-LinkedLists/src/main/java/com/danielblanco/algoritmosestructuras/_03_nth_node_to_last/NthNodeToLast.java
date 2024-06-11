0781ecc016415c2fe473ab0c8da14d1e5a3969c1
jhecohe <jhecohe@gmail.com>
2024-06-10 22:08:06 -0500

    Nth Node to last solved

--- 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_03_nth_node_to_last/NthNodeToLast.java
+++ 04-LinkedLists/src/main/java/com/danielblanco/algoritmosestructuras/_03_nth_node_to_last/NthNodeToLast.java
@@ -2,16 +2,57 @@ package com.danielblanco.algoritmosestructuras._03_nth_node_to_last;
 
 import com.danielblanco.algoritmosestructuras._00_linkedlist.Node;
 
+import java.util.Queue;
+
 /*
  * Dada una lista enlazada simple y un valor N, devuelve el nodo N empezando por el final
  *
  * Ejemplo:
  *  Input: 1->2->4->6, 2
  *  Output: 4
+ *
+ * First solution is iterating over the list and subtract the number of elements
+ *
+ * Optimized solution, use two pointers
  */
 public class NthNodeToLast {
 
   public Node nthNodeToLast(Node head, int n) {
-    throw new UnsupportedOperationException("Not implemented yet");
+
+    Node p1 = head;
+    Node p2 = head;
+
+    for (int i = 0; i < n; i++) {
+      if (p1 == null) return null;
+      p1 = p1.next;
+    }
+
+    while(p1 != null) {
+      p1 = p1.next;
+      p2 = p2.next;
+    }
+
+    return p2;
   }
+
+//  public Node nthNodeToLast(Node head, int n) {
+//
+//    Node temp = head;
+//    int count = 0;
+//    while (temp != null) {
+//      count++;
+//      temp = temp.next;
+//    }
+//
+//    if( count < n) return null;
+//
+//    int index = count - n;
+//
+////    Node select = null;
+//    for (int i = 0; i < index; i++) {
+//      head = head.next;
+//    }
+//
+//    return head;
+//  }
 }
