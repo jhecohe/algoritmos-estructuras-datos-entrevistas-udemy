39c4a16f221adb6ff7d5e38ef6b453d7d8c53e59
jhecohe <jhecohe@gmail.com>
2024-06-24 06:38:01 -0500

    Stack min solved

--- 05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_04_stack_min/StackMin.java
+++ 05-StacksQueues/src/main/java/com/danielblanco/algoritmosestructuras/_04_stack_min/StackMin.java
@@ -1,20 +1,33 @@
 package com.danielblanco.algoritmosestructuras._04_stack_min;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /*
  * ¿Cómo diseñarías un Stack que además de las operaciones de push y pop también
  * contase con una operación para obtener el mínimo?
  */
 public class StackMin {
 
+  private Deque<Integer> valuesStack = new ArrayDeque<>();
+  private Deque<Integer> minsStack = new ArrayDeque<>();
+
   public void push(Integer data) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    valuesStack.push(data);
+    if (minsStack.isEmpty() || data < minsStack.peek()) {
+      minsStack.push(data);
+    }
   }
 
   public int pop() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    int value = valuesStack.pop();
+    if (value == minsStack.peek()){
+      minsStack.pop();
+    }
+    return value;
   }
 
   public int min() {
-    throw new UnsupportedOperationException("Not implemented yet");
+    return minsStack.peek();
   }
 }
