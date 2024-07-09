90e09efad84686090b159d71679898949cade427
jhecohe <jhecohe@gmail.com>
2024-07-09 05:43:05 -0500

    Exercise 5 tree solved

--- 06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_05_is_subtree/IsSubTree.java
+++ 06-Trees/src/main/java/com/danielblanco/algoritmosestructuras/trees/_05_is_subtree/IsSubTree.java
@@ -38,6 +38,37 @@ import com.danielblanco.algoritmosestructuras.trees._00_binarytree.Node;
 public class IsSubTree {
 
   public boolean isSubtree(Node first, Node second) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    StringBuilder sb1 = new StringBuilder();
+    StringBuilder sb2 = new StringBuilder();
+
+    preOrder(first, sb1);
+    preOrder(second, sb2);
+
+    return sb1.toString().contains(sb2.toString());
   }
+
+  public void preOrder( Node node, StringBuilder sb) {
+
+    if (node == null){
+      sb.append("X");
+      return;
+    }
+    sb.append(node.value);
+
+    preOrder(node.left, sb);
+    preOrder(node.right, sb);
+  }
+
+//  public boolean isSubtree(Node first, Node second) {
+//
+//    if (first == null && second == null) return true;
+//
+//    boolean root = first.value == second.value;
+//    boolean left = first.left.value == second.left.value;
+//    boolean right = first.right.value == second.right.value;
+//
+//    if (!root && !left && !right) return false;
+//
+//    return isSubtree(first.left, second.left) && isSubtree(first.right, second.right);
+//  }
 }
